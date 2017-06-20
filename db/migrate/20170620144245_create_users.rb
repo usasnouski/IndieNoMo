4a4ee3b8f3c67fe2a86a0b0e8e2f8b74902a6d58
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-06-20 11:00:15 -0400

    Migrate Users

---
+++ db/migrate/20170620144245_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.timestamps null: false
+    end
+
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+User.delete_all
+
+User.create!(
+  email: "user@email.com",
+  password: "starward"
+)
