aa708736fdac35457123e4fd9fb8a7de5c90a9fd
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-06-20 09:34:08 -0400

    Initital backend setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+  end
+
+  root "static_pages#root"
 end
