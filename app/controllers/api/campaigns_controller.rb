4547ec5a6c5ec390cb3c109d999bcee9508465ae
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-06-22 14:57:05 -0400

    Create initial campaign controllers

---
+++ app/controllers/api/campaigns_controller.rb
@@ -0,0 +1,62 @@
+class Api::SessionsController < ApplicationController
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def show
+    @campaign = campaign
+    if @campaign
+      render :show
+    else
+      render json: "No campaign found", status: 422
+    end
+  end
+
+  def create
+    unless logged_in?
+      render json: "Please sign up or log in", status: 401
+      return
+    end
+
+    @campaign = Campaign.new(campaign_params)
+    campaign.user_id = current_user.id
+    if @campaign.save
+      render :show
+    else
+      render json: @campaign.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @campaign = Campaign.find(params[:id])
+
+    unless logged_in?
+      render json: "Please sign up or log in", status: 401
+      return
+    end
+
+    #can user edit other users' campaigns
+
+    if @campaign.update(campaign_params)
+      render :show
+    else
+      render json: @campaign.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @campaign = Campaign.find(params[:id])
+    @campaign.destroy
+    render :show
+  end
+
+  private
+
+  def campaign_params
+    params
+      .require(:campaign)
+      .permit(
+      :title, :tagline, :description, :overview, :goal_amount, :end_date
+      )
+  end
+end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -18,6 +18,7 @@
 class Campaign < ApplicationRecord
   validates :title, :tagline, :description, :overview, :goal_amount,
             :enddate, :user_id, presence: true
+  validates :title, length: { maximum: 50, allow_nil: true }
   #add category_id to validation
 
   belongs_to :user
