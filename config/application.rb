4a343a9849c2fec7f07e12586076555320c7637b
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-06-26 13:40:37 -0400

    Change in paperclip configs

--- config/application.rb
+++ config/application.rb
@@ -11,30 +11,20 @@ module IndieNoMo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-  #   config.paperclip_defaults = {
-  #     :storage => :s3,
-  #     :s3_credentials => {
-  #       :bucket => ENV["s3_bucket"],
-  #       :access_key_id => ENV["s3_access_key_id"],
-  #       :secret_access_key => ENV["s3_secret_access_key"],
-  #       :s3_region => ENV["s3_region"]
-  #   }
-  # }
+  config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_credentials => {
+          :bucket => ENV["s3_bucket"],
+          :access_key_id => ENV["s3_access_key_id"],
+          :secret_access_key => ENV["s3_secret_access_key"],
+          s3_host_name: "s3-#{ENV['s3_region']}.amazonaws.com",
+          :s3_region => ENV["s3_region"],
+          :url => ":s3_host_name"
+        }
+      }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
-
-config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-        :bucket => ENV["s3_bucket"],
-        :access_key_id => ENV["s3_access_key_id"],
-        :secret_access_key => ENV["s3_secret_access_key"],
-        s3_host_name: "s3-#{ENV['s3_region']}.amazonaws.com",
-        :s3_region => ENV["s3_region"],
-        :url => ":s3_host_name"
-      }
-    }
