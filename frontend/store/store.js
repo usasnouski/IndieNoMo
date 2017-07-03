ef7771ec724dd2671c65092894487058d53b4a3e
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-07-03 14:22:12 -0400

    CreateLogger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,15 +4,18 @@ import thunk from 'redux-thunk';
 
 import RootReducer from '../reducers/root_reducer';
 
-if (process.env.NODE_ENV !== 'production') {
-  middlewares.push(require('redux-logger')());
-}
+const middlewares = [thunk];
 
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { createLogger } = require('redux-logger');
+  middlewares.push(createLogger());
+}
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 
