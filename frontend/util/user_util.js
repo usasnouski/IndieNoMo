e50579f3c75a93482e82cb1480c999ef30a8ef10
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-07-24 16:59:52 -0400

    Fix users util

--- frontend/util/user_util.js
+++ frontend/util/user_util.js
@@ -8,6 +8,6 @@ export const fetchUser = (id) => {
 export const fetchUserInfo = (id) => {
   return $.ajax({
     method: 'GET',
-    url: `api/users/${id}/campaigns`
+    url: `api/users/${id}/info`
   });
 };
